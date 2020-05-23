476d7f821da60d1e6dde6a48c7fbbb83a0c7fb5c
JulhaMouraR <65780193+JulhaMouraR@users.noreply.github.com>
2020-05-23 12:44:41 -0300

    Update Hackathon.py

--- Hackathon.py
+++ Hackathon.py
@@ -1,2 +1,16 @@
-import Sqlite3
-        
+#AGENDAMENTO
+import datetime
+
+c = 0
+cont= +1
+nome = input('Nome do aluno:')
+while (len(nome)<6 or len(nome)>30):
+    nome=input('Digite seu nome novamente:')
+data = input('Data de nascimento:')
+
+while (len(nome)<6 or len(nome)>30):
+    nome=input('Digite seu nome novamente:')
+CPF = int(input('Insira seu CPF:'))
+responsavel = input('Nome do responsável:')
+escola= input('Insira o nome da escola:')
+turno = input('Insira o turno:')
