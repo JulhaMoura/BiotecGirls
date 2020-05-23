4f831fe8376ffde6b16f0f13db53f3f2b0ae6410
JulhaMouraR <65780193+JulhaMouraR@users.noreply.github.com>
2020-05-23 12:54:25 -0300

    Update Hackathon.py

--- Hackathon.py
+++ Hackathon.py
@@ -1,16 +1,14 @@
-#AGENDAMENTO
-import datetime
-
 c = 0
 cont= +1
 nome = input('Nome do aluno:')
 while (len(nome)<6 or len(nome)>30):
     nome=input('Digite seu nome novamente:')
-data = input('Data de nascimento:')
-
-while (len(nome)<6 or len(nome)>30):
-    nome=input('Digite seu nome novamente:')
+data = input('Data de nascimento(Coloque as barras):')
 CPF = int(input('Insira seu CPF:'))
+while (len(CPF)<11 or len(CPF)>11):
+    nome=input('Digite seu CPF novamente:')
 responsavel = input('Nome do responsável:')
 escola= input('Insira o nome da escola:')
 turno = input('Insira o turno:')
+while (len(turno)!=manhã or len(turno)!=tarde or len(turno)!=noite):
+    nome=input('Digite seu turno novamente:')
