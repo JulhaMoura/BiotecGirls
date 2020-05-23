b66dd08bc60c959e50632c4a6621eb40604dcb5b
JulhaMouraR <65780193+JulhaMouraR@users.noreply.github.com>
2020-05-23 15:56:00 -0300

    Update Hackathon.py

--- Hackathon.py
+++ Hackathon.py
@@ -1,14 +1,19 @@
 c = 0
 cont= +1
 nome = input('Nome do aluno:')
-while (len(nome)<6 or len(nome)>30):
+while (len(nome)<6 or len(nome)>100):
     nome=input('Digite seu nome novamente:')
-data = input('Data de nascimento(Coloque as barras):')
+dia = input('Digite o dia do seu nascimento:')
+while (len(dia)<1 or len(dia)>31):
+     dia=input('Digite o dia novamente:')
+mês = input('Digite seu mês:')
+while (len(mês) != [1,2,3,4,5,6,7,8,9,10,11,12]):
+     mês=input('Digite o mês novamente:')
 CPF = int(input('Insira seu CPF:'))
 while (len(CPF)<11 or len(CPF)>11):
-    nome=input('Digite seu CPF novamente:')
+    CPF=input('Digite seu CPF novamente:')
 responsavel = input('Nome do responsável:')
 escola= input('Insira o nome da escola:')
 turno = input('Insira o turno:')
 while (len(turno)!=manhã or len(turno)!=tarde or len(turno)!=noite):
-    nome=input('Digite seu turno novamente:')
+    turno=input('Digite seu turno novamente:')
