6cf67daa82b11e3b3506ba1dffbe86e0423baecd
JulhaMouraR <65780193+JulhaMouraR@users.noreply.github.com>
2020-05-24 00:42:26 -0300

    Update Hackathon.py

--- Hackathon.py
+++ Hackathon.py
@@ -5,9 +5,10 @@ cursor = conn.cursor()
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS students(
 UserID VARCHAR(20) NOT NULL,
-username VARCHAR(20) NOT NULL,
-firstname VARCHAR(20) NOT NULL,
-password VARCHAR(20) NOT NULL);
+username VARCHAR(60) NOT NULL,
+firstname VARCHAR(60) NOT NULL,
+email VARCHAR(60) NOT NULL,
+password VARCHAR(80) NOT NULL);
 ''')
 
 #gravando no bd
@@ -17,13 +18,14 @@ conn.commit()
 p_1 = input('UserID: ')
 p_2 = input('username: ')
 p_3 = input('firstname: ')
-p_4 = input('password: ')
+p_4 = input('email: ')
+p_5 = input('password: ')
 
 #inserindo informações
 cursor.execute('''
 INSERT INTO students(UserID, username, firstname, password)
 VALUES(?,?,?,?)
-''', (p_1, p_2, p_3, p_4))
+''', (p_1, p_2, p_3, p_4, p_5))
 
 print('Dados inseridos com sucesso!!!')
 
